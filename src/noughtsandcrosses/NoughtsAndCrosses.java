851eb02ae576e9015436d96e8ba1221dbb50697d
Alex-Yates <ajy.mail@gmail.com>
2015-07-12 19:50:55 +0100

    Added takeTurn() method which prompts the user to tell the game where they wish to go. Also some basic game play to main().
    Next need to add logic for when one player overrides another players move and some logic to determine if there is a winner.

--- src/noughtsandcrosses/NoughtsAndCrosses.java
+++ src/noughtsandcrosses/NoughtsAndCrosses.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package noughtsandcrosses;
+
 import java.util.*;
+import java.io.*;
+
 /**
  *
  * @author Alex
@@ -12,41 +15,90 @@ import java.util.*;
 public class NoughtsAndCrosses {
 
     /**
-     * 
+     *
      */
     static boolean turn = true;
     static List grid = new ArrayList();
-      
+
     public static void main(String[] args) {
         initialiseGrid();
-        printGrid();
+        for (int turn = 0; turn < 9; turn++) {
+            takeTurn();
+            printGrid();
+            nextPlayer();
+        }
     }
-    
-    public static boolean turnIsTrue (){
+
+    public static boolean turnIsTrue() {
         return (turn == true);
     }
-    
-    public static void nextPlayer (){
-        if (turnIsTrue()){
+
+    public static void nextPlayer() {
+        if (turnIsTrue()) {
             turn = false;
-        }
-        else {
+        } else {
             turn = true;
         }
     }
-    
-    public static void initialiseGrid(){
-        for (int i = 0; i < 9; i++){
+
+    public static void initialiseGrid() {
+        for (int i = 0; i < 9; i++) {
             grid.add(" ");
         }
     }
-    
-    public static void printGrid(){
+
+    public static void printGrid() {
         System.out.println(" " + grid.get(0) + " | " + grid.get(1) + " | " + grid.get(2) + " ");
         System.out.println("---+---+---");
         System.out.println(" " + grid.get(3) + " | " + grid.get(4) + " | " + grid.get(5) + " ");
         System.out.println("---+---+---");
         System.out.println(" " + grid.get(6) + " | " + grid.get(7) + " | " + grid.get(8) + " ");
-}   }
+    }
 
+    public static void takeTurn() {
+        int move;
+        boolean legalMove = false;
+        // Copied some of the code for this section from 
+        // http://www.avajava.com/tutorials/lessons/
+        // how-do-i-accept-text-input-in-a-console-application.html 
+        try {
+            while (legalMove == false) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                if (turnIsTrue()) {
+                    System.out.println("Crosses turn next:");
+                } else {
+                    System.out.println("Noughts turn next:");
+                }
+                // I'm not sure why but without using line.separator
+                // (below) there was no new line after:
+                // "Where would you like to go?"
+                String newLine = System.getProperty("line.separator");
+                System.out.print("Where would you like to go?" + newLine);
+                System.out.println(" 0 | 1 | 2 ");
+                System.out.println("---+---+---");
+                System.out.println(" 3 | 4 | 5 ");
+                System.out.println("---+---+---");
+                System.out.println(" 6 | 7 | 8 ");
+                System.out.print("Enter a number from 0-8:");
+                String str = br.readLine();
+                if (str.equals("0") || str.equals("1") || str.equals("2")
+                        || str.equals("3") || str.equals("4") || str.equals("5")
+                        || str.equals("6") || str.equals("7") || str.equals("8")) {
+                    move = Integer.parseInt(str);
+                    if (turnIsTrue()) {
+                        grid.set(move, "X");
+                    } else {
+                        grid.set(move, "O");
+                    }
+                    legalMove = true;
+                } else {
+                    System.out.println("You must enter an integer between 0-8.");
+                }
+            }
+        } catch (IOException e) {
+            // e.printStackTrace();
+            System.out.println(e);
+        }
+    }
 
+}
