3f48955f7676ec167042c37075786c97b8474eb2
Alex-Yates <ajy.mail@gmail.com>
2015-07-13 19:45:12 +0100

    I was getting compile errors in isWon() function because grid.get(0) could not be assigned to winner. Fixed it using a cast. Also NetBeans was complaining in the same method that I should split the declaration and assignment of winner.

--- src/noughtsandcrosses/NoughtsAndCrosses.java
+++ src/noughtsandcrosses/NoughtsAndCrosses.java
@@ -58,39 +58,40 @@ public class NoughtsAndCrosses {
     public static boolean isWon() {
         boolean gameIsWon = false;
         String winner;
+        winner = "";
         
         //Determining if victory conditions are met
         if (grid.get(0).equals(grid.get(1)) && grid.get(1).equals(grid.get(2)) && !grid.get(0).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(0);
+            winner = (String) grid.get(0);
         }
         else if (grid.get(3).equals(grid.get(4)) && grid.get(4).equals(grid.get(5)) && !grid.get(3).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(3);
+            winner = (String) grid.get(3);
         }
         else if (grid.get(6).equals(grid.get(7)) && grid.get(7).equals(grid.get(8)) && !grid.get(6).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(6);
+            winner = (String) grid.get(6);
         }
         else if (grid.get(0).equals(grid.get(3)) && grid.get(3).equals(grid.get(6)) && !grid.get(0).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(0);
+            winner = (String) grid.get(0);
         }
         else if (grid.get(1).equals(grid.get(4)) && grid.get(4).equals(grid.get(7)) && !grid.get(1).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(1);
+            winner = (String) grid.get(1);
         }
         else if (grid.get(2).equals(grid.get(5)) && grid.get(5).equals(grid.get(8)) && !grid.get(2).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(2);
+            winner = (String) grid.get(2);
         }
         else if (grid.get(0).equals(grid.get(4)) && grid.get(4).equals(grid.get(8)) && !grid.get(0).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(0);
+            winner = (String) grid.get(0);
         }
         else if (grid.get(2).equals(grid.get(4)) && grid.get(4).equals(grid.get(6)) && !grid.get(2).equals(" ")){
             gameIsWon = true;
-            winner = grid.get(0);
+            winner = (String) grid.get(0);
         }
         
         // Logging the winner 
