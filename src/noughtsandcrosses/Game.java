8acb68fb8a11bf9a85fdfc70ad76f88d55f6dbf5
alex.yates <alex.yates@lt-salesdemo1.red-gate.com>
2015-07-14 13:43:00 +0100

    Created an iPlayer interface that will allow me to create a ComputerPlayer Class in the future to implement AI.
    Much refactoring.

--- src/noughtsandcrosses/Game.java
+++ src/noughtsandcrosses/Game.java
@@ -5,8 +5,6 @@
  */
 package noughtsandcrosses;
 
-import java.io.*;
-import java.util.*;
 
 /**
  *
@@ -17,8 +15,9 @@ public class Game {
      * Class variables used to hold the identity of the current player and the
      * current state of the grid.
      */
-    private String currentPlayer;
-    private List grid;
+    private iPlayer currentPlayer;
+    private iPlayer player1;
+    private iPlayer player2;
     
     /**
      * Constuctor for Game objects.
@@ -26,38 +25,36 @@ public class Game {
      * the initialiseGrid() method to initialise grid.
      */
     public Game(){
-        super();
-        this.currentPlayer = "O";
-        this.grid = new ArrayList();
+        super();  
         this.initialiseGrid();
+        player1 = new HumanPlayer("Alex", "X");
+        player2 = new HumanPlayer("Emma", "O");
+        this.currentPlayer = this.player1;
+    }
+
+    public iPlayer getCurrentPlayer(){
+        return currentPlayer;
     }
     
-    /**
-     * Returns true if current player is X
-     * @return true if current player is X
-     */
-    private boolean turnIsX() {
-        return (currentPlayer.equals("X"));
-    }
-
-    /**
-     * Swaps the current player for the next turn
-     */
-    public void nextPlayer() {
-        if (turnIsX()) {
-            currentPlayer = "O";
-        } else {
-            currentPlayer = "X";
-        }
-    }
-
     /**
      * Initialises the playing grid to a blank slate. All values from 0 - 8 will
      * be set to " ".
      */
     private void initialiseGrid() {
+        NoughtsAndCrosses.grid.clear();
         for (int i = 0; i < 9; i++) {
-            grid.add(" ");
+            NoughtsAndCrosses.grid.add(" ");
+        }
+    }
+    
+    /**
+     * Swaps the current player for the next turn
+     */
+    public void nextPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
         }
     }
 
@@ -65,11 +62,11 @@ public class Game {
      * Logs the current state of the grid.
      */
     public void printGrid() {
-        System.out.println(" " + grid.get(0) + " | " + grid.get(1) + " | " + grid.get(2) + " ");
+        System.out.println(" " + NoughtsAndCrosses.grid.get(0) + " | " + NoughtsAndCrosses.grid.get(1) + " | " + NoughtsAndCrosses.grid.get(2) + " ");
         System.out.println("---+---+---");
-        System.out.println(" " + grid.get(3) + " | " + grid.get(4) + " | " + grid.get(5) + " ");
+        System.out.println(" " + NoughtsAndCrosses.grid.get(3) + " | " + NoughtsAndCrosses.grid.get(4) + " | " + NoughtsAndCrosses.grid.get(5) + " ");
         System.out.println("---+---+---");
-        System.out.println(" " + grid.get(6) + " | " + grid.get(7) + " | " + grid.get(8) + " ");
+        System.out.println(" " + NoughtsAndCrosses.grid.get(6) + " | " + NoughtsAndCrosses.grid.get(7) + " | " + NoughtsAndCrosses.grid.get(8) + " ");
     }
 
     /**
@@ -86,35 +83,39 @@ public class Game {
         winner = "";
 
         //Determining if victory conditions are met
-        if (grid.get(0).equals(grid.get(1)) && grid.get(1).equals(grid.get(2)) && !grid.get(0).equals(" ")) {
+        if (NoughtsAndCrosses.grid.get(0).equals(NoughtsAndCrosses.grid.get(1)) && NoughtsAndCrosses.grid.get(1).equals(NoughtsAndCrosses.grid.get(2)) && !NoughtsAndCrosses.grid.get(0).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(0);
-        } else if (grid.get(3).equals(grid.get(4)) && grid.get(4).equals(grid.get(5)) && !grid.get(3).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(3).equals(NoughtsAndCrosses.grid.get(4)) && NoughtsAndCrosses.grid.get(4).equals(NoughtsAndCrosses.grid.get(5)) && !NoughtsAndCrosses.grid.get(3).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(3);
-        } else if (grid.get(6).equals(grid.get(7)) && grid.get(7).equals(grid.get(8)) && !grid.get(6).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(6).equals(NoughtsAndCrosses.grid.get(7)) && NoughtsAndCrosses.grid.get(7).equals(NoughtsAndCrosses.grid.get(8)) && !NoughtsAndCrosses.grid.get(6).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(6);
-        } else if (grid.get(0).equals(grid.get(3)) && grid.get(3).equals(grid.get(6)) && !grid.get(0).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(0).equals(NoughtsAndCrosses.grid.get(3)) && NoughtsAndCrosses.grid.get(3).equals(NoughtsAndCrosses.grid.get(6)) && !NoughtsAndCrosses.grid.get(0).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(0);
-        } else if (grid.get(1).equals(grid.get(4)) && grid.get(4).equals(grid.get(7)) && !grid.get(1).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(1).equals(NoughtsAndCrosses.grid.get(4)) && NoughtsAndCrosses.grid.get(4).equals(NoughtsAndCrosses.grid.get(7)) && !NoughtsAndCrosses.grid.get(1).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(1);
-        } else if (grid.get(2).equals(grid.get(5)) && grid.get(5).equals(grid.get(8)) && !grid.get(2).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(2).equals(NoughtsAndCrosses.grid.get(5)) && NoughtsAndCrosses.grid.get(5).equals(NoughtsAndCrosses.grid.get(8)) && !NoughtsAndCrosses.grid.get(2).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(2);
-        } else if (grid.get(0).equals(grid.get(4)) && grid.get(4).equals(grid.get(8)) && !grid.get(0).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(0).equals(NoughtsAndCrosses.grid.get(4)) && NoughtsAndCrosses.grid.get(4).equals(NoughtsAndCrosses.grid.get(8)) && !NoughtsAndCrosses.grid.get(0).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(0);
-        } else if (grid.get(2).equals(grid.get(4)) && grid.get(4).equals(grid.get(6)) && !grid.get(2).equals(" ")) {
+            winner = this.currentPlayer.getName();
+        } else if (NoughtsAndCrosses.grid.get(2).equals(NoughtsAndCrosses.grid.get(4)) && NoughtsAndCrosses.grid.get(4).equals(NoughtsAndCrosses.grid.get(6)) && !NoughtsAndCrosses.grid.get(2).equals(" ")) {
             gameIsWon = true;
-            winner = (String) grid.get(0);
+            winner = this.currentPlayer.getName();
         }
 
-        // Logging the winner 
+        /**
+         * Logging the winner
+         * Note: this only works if you know the game has
+         * been won by currentPlayer
+         */ 
         if (gameIsWon) {
-            System.out.println("Game over! " + winner + "'s win!");
+            System.out.println("Game over! " + winner + " wins!");
         }
 
         // Signalling whether the game is over
@@ -130,7 +131,7 @@ public class Game {
      * @return True if the game is drawn. False if not.
      */
     private boolean isDraw() {
-        if (!isWon() && !grid.contains(" ")) {
+        if (!isWon() && !NoughtsAndCrosses.grid.contains(" ")) {
             System.out.println("It's a draw!");
             return true;
         }
@@ -145,56 +146,4 @@ public class Game {
     public boolean gameEnd() {
         return (isWon() || isDraw());
     }
-
-    /**
-     * Asks the next player where they would like to go. Then reads the input
-     * from the command prompt and updates grid<> accordingly.
-     */
-    public void takeTurn() {
-        int move;
-        boolean legalMove = false;
-        // Copied some of the code for this section from 
-        // http://www.avajava.com/tutorials/lessons/
-        // how-do-i-accept-text-input-in-a-console-application.html 
-        try {
-            while (legalMove == false) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                if (turnIsX()) {
-                    System.out.println("Crosses turn next:");
-                } else {
-                    System.out.println("Noughts turn next:");
-                }
-                // I'm not sure why but without using line.separator
-                // (below) there was no new line after:
-                // "Where would you like to go?"
-                String newLine = System.getProperty("line.separator");
-                System.out.print("Where would you like to go?" + newLine);
-                System.out.println(" 0 | 1 | 2 ");
-                System.out.println("---+---+---");
-                System.out.println(" 3 | 4 | 5 ");
-                System.out.println("---+---+---");
-                System.out.println(" 6 | 7 | 8 ");
-                System.out.print("Enter a number from 0-8:");
-                String str = br.readLine();
-                if (str.equals("0") || str.equals("1") || str.equals("2")
-                        || str.equals("3") || str.equals("4") || str.equals("5")
-                        || str.equals("6") || str.equals("7") || str.equals("8")) {
-                    move = Integer.parseInt(str);
-                    if (grid.get(move).equals(" ")) {
-                        grid.set(move, currentPlayer);
-                        legalMove = true;
-                    } else {
-                        System.out.println("The space must be free!");
-                    }
-                } else {
-                    System.out.println("You must enter an integer between 0-8.");
-
-                }
-            }
-        } catch (IOException e) {
-            // e.printStackTrace();
-            System.out.println(e);
-        }
-    }
-
 }
